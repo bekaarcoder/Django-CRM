8d7216b34ef6b8d57f2064c01e7ce81246870756
Shashank <s_shashank@outlook.com>
2021-02-02 22:19:29 +0530

    added template

--- leads/models.py
+++ leads/models.py
@@ -27,4 +27,7 @@ class Lead(models.Model):
 class Agent(models.Model):
   user = models.OneToOneField(User, on_delete=models.CASCADE)
 
+  def __str__(self):
+    return self.user.email
+
   
--- leads/views.py
+++ leads/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+from .models import Lead
+
+def home_page(request):
+  leads = Lead.objects.all()
+  context = {
+    "leads": leads
+  }
+  return render(request, 'home_page.html', context)
