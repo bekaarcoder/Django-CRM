c1b11ff62505bbc52d948db0aa477808d4732fd6
Shashank <s_shashank@outlook.com>
2021-02-07 22:16:58 +0530

    setting auth permissions and signals

--- leads/views.py
+++ leads/views.py
@@ -1,5 +1,6 @@
 from django.core.mail import send_mail
 from django.shortcuts import render, redirect, reverse
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import HttpResponse
 from django.views.generic import TemplateView, ListView, DetailView, UpdateView, DeleteView, CreateView
 from .models import Lead, Agent
@@ -18,7 +19,7 @@ class LandingPageView(TemplateView):
 def landing_page(request):
   return render(request, 'landing.html')
 
-class LeadListView(ListView):
+class LeadListView(LoginRequiredMixin, ListView):
   template_name = 'leads/lead_list.html'
   queryset = Lead.objects.all()
   context_object_name = "leads"
@@ -32,7 +33,7 @@ def lead_list(request):
   return render(request, 'leads/lead_list.html', context)
 
 
-class LeadDetailView(DetailView):
+class LeadDetailView(LoginRequiredMixin, DetailView):
   template_name = 'leads/lead_detail.html'
   queryset = Lead.objects.all()
   context_object_name = 'lead'
@@ -46,7 +47,7 @@ def lead_detail(request, pk):
   return render(request, 'leads/lead_detail.html', context)
 
 
-class LeadCreateView(CreateView):
+class LeadCreateView(LoginRequiredMixin, CreateView):
   template_name = 'leads/lead_create.html'
   form_class = LeadModelForm
 
@@ -79,7 +80,7 @@ def lead_create(request):
   return render(request, 'leads/lead_create.html', context)
 
 
-class LeadUpdateView(UpdateView):
+class LeadUpdateView(LoginRequiredMixin, UpdateView):
   template_name = 'leads/lead_update.html'
   queryset = Lead.objects.all()
   form_class = LeadModelForm
@@ -106,7 +107,7 @@ def lead_update(request, pk):
   return render(request, 'leads/lead_update.html', context)
 
 
-class LeadDeleteView(DeleteView):
+class LeadDeleteView(LoginRequiredMixin, DeleteView):
   template_name = 'leads/lead_delete.html'
   queryset = Lead.objects.all()
 
